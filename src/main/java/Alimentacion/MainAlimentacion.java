6a9e29d666a1fd84cd7762a9feed6d3a63d2679b
Jeremy Fuentes <jeremyfuentes201@gmail.com>
2025-03-15 21:56:57 -0600

    Archivo Main para Alimentacion y para Descontable

--- src/main/java/Alimentacion/MainAlimentacion.java
+++ src/main/java/Alimentacion/MainAlimentacion.java
@@ -1,7 +1,15 @@
 
 package Alimentacion;
-
+import Services.IInterfazAlimentacion;
 
 public class MainAlimentacion {
-    
+    public static void main(String[] args) {
+
+        IInterfazAlimentacion<String> persona = new Persona();
+        System.out.println(persona.Comer(""));  
+
+        IInterfazAlimentacion<String> animal = new Animal();
+        System.out.println(animal.Comer("")); 
+        
+    }
 }
--- src/main/java/Descontable/MainDescuento.java
+++ src/main/java/Descontable/MainDescuento.java
@@ -6,10 +6,10 @@ import Services.IInterfazDescontable;
 public class MainDescuento {
     public static void main(String[] args) {
         IInterfazDescontable<Double> descuento1 = new DescuentoPorcentaje(15.5);
-        System.out.println("Descuento del 15.5%: " + descuento1.calcularDescuento(200.0)); // 31.0
+        System.out.println("Descuento del 15.5%: " + descuento1.calcularDescuento(200.0)); 
 
         IInterfazDescontable<Double> descuento2 = new DescuentoFijo(25.75);
-        System.out.println("Precio con descuento fijo de 25.75: " + descuento2.calcularDescuento(200.0)); // 174.25
+        System.out.println("Precio con descuento fijo de 25.75: " + descuento2.calcularDescuento(200.0));
     }
 }
 
